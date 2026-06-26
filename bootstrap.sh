88bad545bcf9172493d5fc998df69fd1e1a30048
mgn <mgn@fortysixandtwo.com>
2026-06-26 15:02:03 -0400

    updated cloudflared install

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,10 +44,9 @@ sudo apt-get update && sudo apt-get install cloudflared
 CF_TOKEN="$1"
 if [ -n "$CF_TOKEN" ]; then
     echo "--> Installing and configuring Cloudflare Tunnel..."
-    # Add cloudflare gpg key
     sudo mkdir -p --mode=0755 /usr/share/keyrings
-    curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
-    echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+    curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg > /dev/null
+    echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
     sudo apt-get update && sudo apt-get install cloudflared
     
     sudo cloudflared service install "$CF_TOKEN"
